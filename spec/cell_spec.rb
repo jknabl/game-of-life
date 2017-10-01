8c4c66de0355330257c8654f5a47d677ac027f75
Jason Knabl <jason.knabl@gmail.com>
2017-10-01 09:46:44 -0400

    Describe cell states in cell spec.

--- spec/cell_spec.rb
+++ spec/cell_spec.rb
@@ -1,5 +1,21 @@
 require 'spec_helper'
 
 describe Cell do 
-    xit 'tests useful things'
+    describe 'cell states' do 
+        context 'when a cell has < 2 live neighbors' do 
+            xit 'is dead' 
+        end
+
+        context 'when a cell has 2 or 3 live neighbors' do 
+            xit 'is alive'
+        end
+
+        context 'when a cell has exactly 3 live neighbors' do 
+            xit 'is alive'
+        end
+
+        context 'when a cell has > 3 live neighbors' do 
+            xit 'is dead'
+        end
+    end
 end
